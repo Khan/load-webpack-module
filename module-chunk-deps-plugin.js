f1ccc49739b28349b2de678136ae5580b626baa0
Kevin Barabash <kevinb@khanacademy.org>
2019-03-20 11:46:16 -0400

    update ModuleChunkMapPlugin to use non-import() module deps to compute a complete and minimal list of bundles need to for a particular module

--- module-chunk-deps-plugin.js
+++ module-chunk-deps-plugin.js
@@ -16,81 +16,79 @@ class ModuleChunkMapPlugin {
     }
 
     apply(compiler) {
-        const pluginName = "FailOnErrorPlugin";
-        let isWatching = false;
+        const pluginName = "ModuleChunkDepsPlugin";
+        // let isWatching = false;
 
-        compiler.hooks.watchRun.tap(pluginName, compiler => {
-            isWatching = true;
-        });
-
+        // compiler.hooks.watchRun.tap(pluginName, compiler => {
+        //     isWatching = true;
+        // });
+        
         compiler.hooks.done.tap(pluginName, stats => {
-            if (!stats.hasErrors()) {
-                for (const module of stats.compilation.modules) {
-                    console.log(module.type);
-                }
-                
-                const chunkMap = {};
-                for (const chunk of stats.compilation.chunks) {
-                    for (const module of [...chunk._modules]) {
-                        chunkMap[module.id] = chunk.id;
-                    }
-                }
-                
-                const moduleDeps = {};
-                for (const chunk of stats.compilation.chunks) {
-                    for (const module of [...chunk._modules]) {
-                        for (const reason of module.reasons) {
-                            if (reason.module) {
-                                // console.log(reason.module.type);
-                                
-                                if (!moduleDeps.hasOwnProperty(reason.module.id)) {
-                                    moduleDeps[reason.module.id] = new Set();
-                                }
-                                moduleDeps[reason.module.id].add(module.id);
-                            }
-                        }
-                    }
-                }
-                
-                const output = {};
-
-                const getChunksForModule = (moduleId) => {
-                    if (output.hasOwnProperty(moduleId)) {
-                        return output[moduleId];
-                    } else {
-                        const containingChunk = chunkMap[moduleId];
-                        const directModuleDeps = [...(moduleDeps[moduleId] || [])];
-                        // compute
-                        const chunkDeps = new Set();
-                        chunkDeps.add(containingChunk);
-                        for (const moduleDep of directModuleDeps) {
-                            for (const chunk of getChunksForModule(moduleDep)) {
-                                chunkDeps.add(chunk);
-                            }
-                        }
-                        // const allDeps = new Set(directModuleDeps.reduce((allDeps, modDepId) => {
-                            
-                        //     return [...allDeps, ...getChunksForModule(modDepId)];
-                        // }, [containingChunk]));
-                        // memoize
-                        output[moduleId] = [...chunkDeps];
-                        // return
-                        return output[moduleId];
-                    }
-                }
-
-                for (const moduleId of Object.keys(moduleDeps)) {
-                    getChunksForModule(moduleId);
-                }
-                
-
-                // for (const [key, deps] of Object.entries(moduleDeps)) {
-                //     const set = new Set([...deps].map(dep => chunkMap[dep]));
-                //     output[key] = [...set];
-                // }
-
-                fs.writeFileSync("./module-chunk-deps.json", JSON.stringify(output, null, 4));
+            if (stats.hasErrors()) {
+                throw new Error("stats.hasErrors");
             }
+
+            const json = stats.toJson();
+            
+            const chunkMap = {};
+            for (const chunk of json.chunks) {
+                for (const module of chunk.modules) {
+                    chunkMap[module.id] = chunk.id;
+                }
+            }
+
+            const moduleDeps = {};
+            for (const module of json.modules) {
+                for (const reason of module.reasons) {
+                    if (reason.moduleId === null) {
+                        continue;
+                    }
+                    // Don't include dynamic imports() since they aren't needed
+                    // to load the module and webpack will load these deps for
+                    // us whenever `import()` is called.
+                    if (reason.type !== "import()") {
+                        if (!moduleDeps.hasOwnProperty(reason.moduleId)) {
+                            moduleDeps[reason.moduleId] = new Set();
+                        }
+                        moduleDeps[reason.moduleId].add(module.id);
+                    }
+                }
+            }
+
+            // for (const [mod, deps] of Object.entries(moduleDeps)) {
+            //     console.log(`${mod}: ${[...deps]}`);
+            // }
+           
+            const output = {};
+
+            const getChunksForModule = (moduleId) => {
+                if (output.hasOwnProperty(moduleId)) {
+                    return output[moduleId];
+                } else {
+                    const containingChunk = chunkMap[moduleId];
+                    const directModuleDeps = [...(moduleDeps[moduleId] || [])];
+                    const chunkDeps = new Set();
+
+                    chunkDeps.add(containingChunk);
+                    for (const moduleDep of directModuleDeps) {
+                        for (const chunk of getChunksForModule(moduleDep)) {
+                            chunkDeps.add(chunk);
+                        }
+                    }
+
+                    // memoize
+                    output[moduleId] = [...chunkDeps];
+ 
+                    // return
+                    return output[moduleId];
+                }
+            }
+
+            for (const moduleId of Object.keys(moduleDeps)) {
+                getChunksForModule(moduleId);
+            }
+
+            fs.writeFileSync("./module-chunk-deps.json", JSON.stringify(output, null, 4));
         });
     }
 }
